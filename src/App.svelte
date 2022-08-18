7043ea3cd53986a750ad3293ff68ccb82691c164
Cosimoyannis <Cosimo.t@live.de>
2022-08-18 10:02:38 +0200

    deploy test

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
   import Umsatzanzeige from './lib/umsatzanzeige.svelte';
   import Content from './lib/content.svelte';
 
